35d476bc580e73d49b818f639944b6b82737a6d3
Rhevan Kruger <rhevankruger@gmail.com>
2024-05-28 17:02:12 +0200

    feature/authentication: created basic ui and routing for welcome page

---
+++ frontend/lib/pages/sing_up.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SingUp extends StatefulWidget {
+  const SingUp({super.key});
+
+  @override
+  State<SingUp> createState() => _SingUpState();
+}
+
+class _SingUpState extends State<SingUp> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: const Scaffold());
+  }
+}
