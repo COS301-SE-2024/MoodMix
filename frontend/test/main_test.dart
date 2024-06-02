516185f0284fb300aba433b9016603ddfe12fbd8
PineCone85 <alexpret85@gmail.com>
2024-06-02 19:34:10 +0200

    Added test3

---
+++ frontend/test/main_test.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:frontend/main.dart' as app;
+import 'package:frontend/pages/loading.dart';
+import 'package:frontend/pages/log_in.dart';
+import 'package:frontend/pages/sing_up.dart';
+import 'package:frontend/pages/welcome.dart';
+
+void main() {
+  testWidgets('Initial Route Test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(app.MyApp());
+
+    // Verify that the initial route is the Loading page.
+    expect(find.byType(Loading), findsOneWidget);
+    expect(find.byType(Welcome), findsNothing);
+    expect(find.byType(SignUp), findsNothing);
+    expect(find.byType(LogIn), findsNothing);
+  });
+
+  testWidgets('Navigate To Welcome', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(app.MyApp());
+
+    // Loading navigates to the Welcome page.
+    await tester.pumpAndSettle();
+
+    // Verify that the Welcome page is displayed.
+    expect(find.byType(Welcome), findsOneWidget);
+    expect(find.byType(Loading), findsNothing);
+    expect(find.byType(SignUp), findsNothing);
+    expect(find.byType(LogIn), findsNothing);
+  });
+}
