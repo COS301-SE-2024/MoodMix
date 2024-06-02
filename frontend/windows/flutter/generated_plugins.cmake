6e73cde7fafd88091f65faf0ad4bf6deccba032c
PineCone85 <110378695+PineCone85@users.noreply.github.com>
2024-06-02 19:19:36 +0200

    Revert "Feature/google sign in"

--- frontend/windows/flutter/generated_plugins.cmake
+++ frontend/windows/flutter/generated_plugins.cmake
@@ -3,11 +3,6 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  firebase_auth
-  firebase_core
-  flutter_secure_storage_windows
-  url_launcher_windows
-  window_to_front
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
