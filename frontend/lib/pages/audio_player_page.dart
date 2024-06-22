51846118aafc7e15acdf35583341a07afd327070
PineCone85 <alexpret85@gmail.com>
2024-06-22 20:20:29 +0200

    Added audio player

---
+++ frontend/lib/pages/audio_player_page.dart
@@ -0,0 +1,162 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+class AudioPlayerPage extends StatefulWidget {
+  @override
+  _AudioPlayerPageState createState() => _AudioPlayerPageState();
+}
+
+class _AudioPlayerPageState extends State<AudioPlayerPage> {
+  late AudioPlayer _audioPlayer;
+  bool isPlaying = false;
+  String audioUrl =
+      'assets/songs/song.mp3'; // Adjust path as per your project structure
+  late Timer _timer;
+  int _currentSeconds = 0;
+  Duration _position = Duration.zero; // To store current playback position
+
+  @override
+  void initState() {
+    super.initState();
+    _audioPlayer = AudioPlayer();
+
+    // Listeners for audio player
+    _audioPlayer.onPlayerCompletion.listen((event) {
+      setState(() {
+        isPlaying = false;
+        _currentSeconds = 0;
+        _position = Duration.zero; // Reset position on completion
+      });
+      _timer.cancel(); // Stop the timer on completion
+    });
+
+    _audioPlayer.onPlayerError.listen((msg) {
+      print('audioPlayer error : $msg');
+      setState(() {
+        isPlaying = false;
+      });
+    });
+
+    // Listen for audio position changes
+    _audioPlayer.onAudioPositionChanged.listen((Duration duration) {
+      setState(() {
+        _currentSeconds = duration.inSeconds;
+      });
+    });
+  }
+
+  void _playPause() async {
+    if (isPlaying) {
+      // Pause the audio
+      await _audioPlayer.pause();
+      _timer.cancel(); // Cancel the timer
+    } else {
+      if (_position == Duration.zero) {
+        // If it's the first play or resumed from the beginning
+        int result = await _audioPlayer.play(audioUrl, isLocal: true);
+        if (result == 1) {
+          // Success
+          _startTimer();
+        } else {
+          // Handle failure to play audio
+          print('Failed to play audio');
+        }
+      } else {
+        // Resume from the stored position
+        int result = await _audioPlayer.seek(_position);
+        if (result == 1) {
+          // Success
+          await _audioPlayer.resume();
+          _startTimer();
+        } else {
+          // Handle failure to resume
+          print('Failed to resume audio');
+        }
+      }
+    }
+    setState(() {
+      isPlaying = !isPlaying;
+      if (!isPlaying) {
+        // Reset timer and current playback position when paused
+        _timer.cancel();
+        _currentSeconds = 0;
+      }
+    });
+  }
+
+  void _startTimer() {
+    const oneSec = const Duration(seconds: 1);
+    _timer = Timer.periodic(
+      oneSec,
+      (Timer timer) {
+        setState(() {
+          if (_currentSeconds < 242) {
+            _currentSeconds++;
+          } else {
+            timer.cancel();
+          }
+        });
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String currentTime = _currentSeconds != null
+        ? '${(_currentSeconds ~/ 60).toString().padLeft(2, '0')}:${(_currentSeconds % 60).toString().padLeft(2, '0')}'
+        : '00:00';
+
+    double progress = (_currentSeconds / 242);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Audio Player'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'assets/images/album_cover.jpg',
+              width: 200,
+              height: 200,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(height: 20),
+            Text(
+              currentTime,
+              style: TextStyle(fontSize: 24.0),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: 250, // Adjust width as per your preference
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: LinearProgressIndicator(
+                  value: progress,
+                  minHeight: 10,
+                  backgroundColor: Colors.grey[300],
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            IconButton(
+              icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
+              iconSize: 64.0,
+              onPressed: _playPause,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    _timer.cancel(); // Cancel the timer when disposing the widget
+    super.dispose();
+  }
+}
--- frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,22 +5,30 @@
 import FlutterMacOS
 import Foundation
 
+import audio_session
+import audioplayers
 import firebase_auth
 import firebase_core
 import flutter_secure_storage_macos
 import flutter_web_auth
 import flutter_web_auth_2
 import google_sign_in_ios
+import just_audio
+import path_provider_foundation
 import url_launcher_macos
 import window_to_front
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  AudioplayersPlugin.register(with: registry.registrar(forPlugin: "AudioplayersPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FlutterWebAuthPlugin.register(with: registry.registrar(forPlugin: "FlutterWebAuthPlugin"))
   FlutterWebAuth2Plugin.register(with: registry.registrar(forPlugin: "FlutterWebAuth2Plugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WindowToFrontPlugin.register(with: registry.registrar(forPlugin: "WindowToFrontPlugin"))
 }
