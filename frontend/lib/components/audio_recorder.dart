a455aa87a2d7e47297e266b91097f6a63d25e800
Rhevan Kruger <RhevanKruger@gmail.com>
2024-08-09 16:36:35 +0200

    feature/finilizationPopups: adapted audio recorder to display text when recording

--- frontend/lib/components/audio_recorder.dart
+++ frontend/lib/components/audio_recorder.dart
@@ -43,30 +43,62 @@ class _AudioRecorderState extends State<AudioRecorder> with SingleTickerProvider
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: _toggleRecording,
-      child: ScaleTransition(
-        scale: _controller.drive(
-          Tween<double>(begin: 1.0, end: 1.2).chain(
-            CurveTween(curve: Curves.elasticOut),
-          ),
-        ),
-        child: Container(
-          width: 80,
-          height: 80,
-          decoration: BoxDecoration(
-            color: isRecording ? Theme.of(context).colorScheme.secondary : Theme.of(context).colorScheme.secondary.withOpacity(0.1),
-            shape: BoxShape.circle,
-          ),
-          child: Center(
-            child: Icon(
-              isRecording ? Icons.stop : Icons.mic,
-              color: Theme.of(context).colorScheme.secondary,
-              size: 40.0,
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Positioned(
+          left: 90,
+          top: 72,
+          child: Container(
+            width: 210,
+            height: 80,
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.secondary.withOpacity(0.2),
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Center(
+              child: Text(
+                isRecording ? 'Recording' : 'Not Recording',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.secondary,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
           ),
         ),
-      ),
+        Positioned(
+          left: 80 - 40,
+          top: 10,
+          bottom: 10,
+          child: GestureDetector(
+            onTap: _toggleRecording,
+            child: ScaleTransition(
+              scale: _controller.drive(
+                Tween<double>(begin: 1.0, end: 1.2).chain(
+                  CurveTween(curve: Curves.elasticOut),
+                ),
+              ),
+              child: Container(
+                width: 80,
+                height: 80,
+                decoration: BoxDecoration(
+                  color: isRecording ? Theme.of(context).colorScheme.secondary : Theme.of(context).colorScheme.secondary.withOpacity(1),
+                  shape: BoxShape.circle,
+                ),
+                child: Center(
+                  child: Icon(
+                    isRecording ? Icons.stop : Icons.mic,
+                    color: Theme.of(context).colorScheme.primary,
+                    size: 40.0,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
