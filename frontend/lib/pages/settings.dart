eda3f30ef2bc565606752f1d4bebdf67e786bd25
Rhevan Kruger <rhevankruger@gmail.com>
2024-09-09 15:58:51 +0200

    feature/realTimeFinalization: re-implemented and fixed theme caching

--- frontend/lib/pages/settings.dart
+++ frontend/lib/pages/settings.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:frontend/theme/theme_provider.dart';
-import 'package:frontend/pages/user_profile.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import '../auth/auth_service.dart';
 import '../theme/theme.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsPage extends StatelessWidget {
   final AuthService _authService = AuthService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
