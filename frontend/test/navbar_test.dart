e2976ce8739b8ccaf49687d197ad8db6cf3bc21e
James <jbd.hardy30@gmail.com>
2024-09-28 15:43:19 +0200

    Home page Widget Working on Navbar Test.

--- frontend/test/navbar_test.dart
+++ frontend/test/navbar_test.dart
@@ -49,18 +49,21 @@ void main() {
 
     // Tap on the bottom navigation item at index 0 (Camera).
     // await tester.tap(find.byIcon(Icons.camera_alt));
-    // await tester.pumpAndSettle(); // Wait for navigation to complete.
-    // verify(mockObserver.didReplace(
-    //     oldRoute: anyNamed('oldRoute'), newRoute: anyNamed('newRoute')));
-    // expect(find.text('Playlist Generation'), findsOneWidget);
+    await tester.tap(find.byKey(Key('CAMERAICON')));
+    await tester.pumpAndSettle(); // Wait for navigation to complete.
+    verify(mockObserver.didReplace(
+        oldRoute: anyNamed('oldRoute'), newRoute: anyNamed('newRoute')));
+    expect(find.text('HOME'), findsOneWidget);
 
-    // // Tap on the bottom navigation item at index 2 (User Profile).
+    // Tap on the bottom navigation item at index 2 (User Profile).
     // await tester.tap(find.byIcon(Icons.person));
     // await tester.pumpAndSettle();
+    // await tester.tap(find.byKey(Key('PROFILEICON')));
     // expect(find.text('PROFILE'), findsOneWidget);
     //
-    // // Tap on the bottom navigation item at index 3 (Settings).
+    // Tap on the bottom navigation item at index 3 (Settings).
     // await tester.tap(find.byIcon(Icons.settings));
+    // await tester.tap(find.byKey(Key('HELPICON')));
     // await tester.pumpAndSettle();
     // expect(find.text('HELP'), findsOneWidget);
   });
